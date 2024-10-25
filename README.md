8351955353771a630637b8bf59f5b79ebc454c36
Yoonsuck Choe <choe@tamu.edu>
2024-10-24 23:00:05 -0500

    originally written with ChatGPT with lots of errors. Rewritten with Claude

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # gmail-hotkey
-gmail hotkey chrome extension
+
+This is a chrome extension to define a custom macro so that a shortcut key can be used to insert configurable text into any text-entry box.
+
--- background.js
+++ background.js
@@ -1,22 +1,50 @@
+
+// background.js
 chrome.commands.onCommand.addListener((command) => {
-  if (command === "insert_text_1") {
-    insertText("No reply usually means no. Repeated reminders are considered unprofessional.");
-  } else if (command === "insert_text_2") {
-    insertText("This is predefined text 2");
+  if (command === 'trigger-macro') {
+    chrome.storage.sync.get(['macros'], async function(result) {
+      const macros = result.macros || {};
+      const macroNames = Object.keys(macros);
+      
+      if (macroNames.length === 0) {
+        return;
+      }
+
+      // Create a list of macro options
+      const options = macroNames.map((name, index) => ({
+        content: name,
+        description: macros[name]
+      }));
+
+      // Get the active tab
+      const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+      
+      // Inject the content script
+      await chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        func: insertMacroText,
+        args: [macros[macroNames[0]]] // Insert first macro by default
+      });
+    });
   }
 });
 
-function insertText(text) {
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    chrome.scripting.executeScript({
-      target: { tabId: tabs[0].id },
-      func: (text) => {
-        const activeElement = document.activeElement;
-        if (activeElement && activeElement.tagName === "INPUT" || activeElement.tagName === "TEXTAREA") {
-          activeElement.value += text;
-        }
-      },
-      args: [text]
-    });
-  });
+
+// Function to be injected into the page
+function insertMacroText(text) {
+  const activeElement = document.activeElement;
+  if (activeElement.tagName === 'INPUT' || activeElement.tagName === 'TEXTAREA' || activeElement.isContentEditable) {
+    const start = activeElement.selectionStart;
+    const end = activeElement.selectionEnd;
+    const value = activeElement.value || activeElement.innerText;
+    const newValue = value.substring(0, start) + text + value.substring(end);
+    
+    if (activeElement.isContentEditable) {
+      activeElement.innerText = newValue;
+    } else {
+      activeElement.value = newValue;
+    }
+    
+    activeElement.selectionStart = activeElement.selectionEnd = start + text.length;
+  }
 }
